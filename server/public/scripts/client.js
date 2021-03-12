4e402cf748dd0061f0d2a2d8fd052e46d5506019
gabinwilliams <scoutsnknives@gmail.com>
2021-03-12 15:08:33 -0600

    created POST route

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,4 +5,43 @@ $(document).ready(handleReady);
 
 function handleReady() {
   console.log('jQ ready!');
+
+  $('#equalsBtn').on('click', sendNumbers);
+
 }
+
+function emptyInputs() {
+
+  $('#num1In').val('');
+  $('#num2In').val('');
+
+};// end emptyInputs
+
+function sendNumbers() {
+
+  let numberObject = {
+
+    num1: $('#num1In').val(),
+    num2: $('#num2In').val()
+
+  };
+
+  console.log('Object to send:', numberObject);
+
+  $.ajax({
+    type: 'POST',
+    url: '/number',
+    data: numberObject
+
+  }).then(function(response) {
+    console.log('Back from POST with:', response);
+
+  }).catch(function(err) {
+
+    console.log(err);
+  });
+
+  emptyInputs();
+
+}// end sendNumbers
+
--- server/server.js
+++ server/server.js
@@ -6,12 +6,25 @@ const bodyParser = require('body-parser');
 
 const PORT = 5000;
 
+let numbersOperations [];
+
 app.use(express.static('server/public'));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 
+// POST Routes
 
+app.post('/number', (req, res) => {
+
+  let numberObject = req.body;
+
+  console.log('In /number POST route');
+  console.log('NumberObject:', numberObject);
+
+  doMathOperation(numberObject);
+
+});
 
 
 
