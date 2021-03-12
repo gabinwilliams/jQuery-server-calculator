be542ff85d2fb79da00f14facfcb073dc7b68662
gabinwilliams <scoutsnknives@gmail.com>
2021-03-11 21:08:20 -0600

    sourced static files in index.html. Server working

---
+++ server/public/scripts/client.js
@@ -0,0 +1,8 @@
+console.log('JS ready');
+
+$(document).ready(handleReady);
+
+
+function handleReady() {
+  console.log('jQ ready!');
+}
---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+
+const bodyParser = require('body-parser');
+
+const PORT = 5000;
+
+app.use(express.static('server/public'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+
+  console.log('listening on port', PORT);
+})
